8fd0389aa79478371bd40e8228980b8fc8e83e45
libanoesposito <libanoesposito@gmail.com>
2025-12-25 16:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -335,7 +335,7 @@ function showLevels(lang) {
 
     for (let i = 1; i <= 5; i++) {
 
-        let label = (i === 5) ? "PROVA A TESTARTI" : "Livello " + i;
+        let label = (i === 5) ? "TEST OPERATIVO" : "Livello " + i;
         let isLocked = false;
 
         // LOGICA UTENTE
