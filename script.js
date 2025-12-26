f225370b5b65c13a799a28411d202ed8b1257229
libanoesposito <libanoesposito@gmail.com>
2025-12-27 00:15:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -316,7 +316,7 @@ function uiPin(type) {
     // Campo nome: lo mostriamo solo se stiamo registrando
     // NOTA: ID cambiato in "reg-name" per farlo leggere a registerUser()
     let nameField = type === 'register' ? 
-    `<input type="text" id="reg-name" class="btn-apple" placeholder="Il tuo Nome" style="text-align:center; margin-bottom:10px; background: rgba(255,255,255,0.05); cursor: text;">` : '';
+    `<input type="text" id="reg-name" class="btn-apple" placeholder="Il tuo Nome" style="text-align:center; margin-bottom:10px; display:block; margin-left:auto; margin-right:auto;">` : '';
 
     // Decidiamo quale funzione chiamare al clic
     let action = type === 'register' ? 'registerUser()' : "validatePin('login')";
@@ -329,7 +329,7 @@ function uiPin(type) {
             ${nameField}
             
             <input type="password" id="reg-pin" class="btn-apple" 
-       style="text-align:center; font-size:24px; letter-spacing:8px; background: rgba(255,255,255,0.05); cursor: text;" 
+       style="text-align:center; font-size:24px; letter-spacing:8px; display:block; margin-left:auto; margin-right:auto;" 
        maxlength="4" inputmode="numeric" placeholder="PIN">
             
             <button class="btn-apple btn-primary" style="margin-top:20px" onclick="${action}">
